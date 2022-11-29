1f0a76e4b4af99b1a6ab01dea191385a7333c3d4
SShym <serh.shym@gmail.com>
2022-11-29 17:55:16 +0200

    routerFix

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser');
 const Database = require('./DB/db'); 
 const app = express();
 const Schema = require('./models/Schema');
-
 const authRouter = require('./routers/authRouter');
 const commentsRouter = require('./routers/commentsRouter')
 
@@ -15,13 +14,6 @@ app.use(cors());
 Database();
 
 app.get('/', (req, res) => { res.send('APP IS RUNNING') });
-
-app.get('/products', async (req, res) => {
-    Schema.find()
-        .exec()
-        .then(products => res.json(products))
-        .catch(err => res.status(500).json(err))
-});
 
 app.use(express.json({limit: '25mb'}));
 app.use(express.urlencoded({limit: '25mb', extended: true}));
