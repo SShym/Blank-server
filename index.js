dfc49e8a079063adc35fbbabe6950dc9a733b2f9
SShym <serh.shym@gmail.com>
2022-11-29 23:50:14 +0200

    changes

--- index.js
+++ index.js
@@ -22,31 +22,6 @@ app.use(express.urlencoded({limit: '25mb', extended: true}));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json()).use(bodyParser.json());
 
-app.get("/:id/verify/:token", async (req, res) => {
-    res.send('MAIL IN VERIRFCATION STATUS')
-    try {
-          const user = await userSchema.findOne({ _id: req.params.id });
-  
-      if (!user) return res.status(400).send({ message: "Invalid user" });
-      
-          const token = await tokenSchema.findOne({
-              userId: req.params.id,
-              token: req.params.token,
-          });
-  
-          if (!token) return res.status(400).send({ message: "Invalid token" });
-      
-          await userSchema.updateOne({ _id: req.params.id}, { verified: true });
-  
-          await token.remove();
-  
-          res.status(201).json({ user });
-      } catch (error) {
-      console.log(error)
-          res.status(500).send({ message: "Internal Server Error" });
-      }
-  });
-
 app.use(authRouter);
 app.use(commentsRouter)
 
