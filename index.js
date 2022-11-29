035271842e5f0411c88f4ff46a9d55f0fa00f2ee
SShym <serh.shym@gmail.com>
2022-11-29 17:51:52 +0200

    ...

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const commentsRouter = require('./routers/commentsRouter')
 
 const PORT = process.env.PORT;
 
+app.use(cors());
+
 Database();
 
 app.get('/', (req, res) => { res.send('APP IS RUNNING') });
@@ -24,7 +26,7 @@ app.get('/products', async (req, res) => {
 app.use(express.json({limit: '25mb'}));
 app.use(express.urlencoded({limit: '25mb', extended: true}));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.json()).use(bodyParser.json()).use(cors());
+app.use(express.json()).use(bodyParser.json());
 
 app.use(authRouter);
 app.use(commentsRouter)
